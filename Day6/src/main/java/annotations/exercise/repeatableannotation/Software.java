8aa44138ac2ed014a2e6c766830dcf70d0267f6d
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-02-18 12:59:47 +0530

    [Pranav] Add. Repeatable Annotations

---
+++ Day6/src/main/java/annotations/exercise/repeatableannotation/Software.java
@@ -0,0 +1,10 @@
+package annotations.exercise.repeatableannotation;
+
+class Software {
+    @BugReport(description = "Null pointer exception occurs on invalid input.")
+    @BugReport(description = "Performance issue when handling large data.")
+    public void processData() {
+        System.out.println("Processing data...");
+    }
+}
+
