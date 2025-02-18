0f9aa2c365aecd80b423e493444619ba0726074c
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-02-18 10:30:10 +0530

    [Pranav] Add. Todo Annotation

---
+++ Day6/src/main/java/annotations/beginnerlevel/todoannotations/Todo.java
@@ -0,0 +1,11 @@
+package annotations.beginnerlevel.todoannotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Todo {
+    String task(); // Description of the task
+    String assignedTo(); // Developer responsible
+    String priority() default "MEDIUM"; // Optional parameter with a default value
+}
